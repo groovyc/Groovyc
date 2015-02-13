3df2ed92dfd89118f0f38ee4a4c2b087007c57ea
groovyc <garveymusic4@gmail.com>
2015-02-13 11:58:15 +0000

    cT1

---
+++ ConsoleApplication3/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+public class Customer
+{
+    private int m_id = -1;
+    private string m_name = string.Empty;
+
+    public Customer(int id, string name)
+    {
+        m_id = id;
+        m_name = name;
+    }
+
+    public int ID
+    {
+        get
+        {
+            return m_id;
+        }
+    }
+
+    public string Name
+    {
+        get
+        {
+            return m_name;
+        }
+    }
+}
+
+public class ReadOnlyCustomerManager
+{
+    public static void Main()
+    {
+        Customer cust = new Customer(1, "Amelio Rosales");
+
+        Console.WriteLine(
+            "ID: {0}, Name: {1}",
+            cust.ID,
+            cust.Name);
+
+        Console.ReadKey();
+    }
+}
